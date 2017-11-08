2a22eaf6f3379f3b6103ced2761609ff1405edaf
Daniel Carlson <dancarl758@gmail.com>
2017-11-08 04:51:06 +0100

    Completed todo-list react app

---
+++ todo-list-react/src/TodoList.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+// Components
+import TodoItems from './TodoItems'
+
+// Styling
+import './TodoList.css';
+
+class TodoList extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function(item) {
+        return (item.key !== key);
+    });
+
+    this.setState({
+      items: filteredItems
+    })
+  }
+
+  addItem(e) {
+    var itemArray = this.state.items;
+
+    if(this._inputElement.value !== "") {
+      itemArray.unshift({
+        text: this._inputElement.value,
+        key: Date.now()
+      });
+
+      this.setState({
+        items: itemArray
+      });
+
+      this._inputElement.value = "";
+    }
+    console.log(itemArray);
+    e.preventDefault(); // <-- What is this?
+  }
+
+
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input
+              placeholder="Enter a task..."
+              ref={(a) => this._inputElement = a}
+            />
+            <button>Add</button>
+          </form>
+        </div>
+        <TodoItems
+          delete={this.deleteItem.bind(this)}
+          entries={this.state.items}
+        />
+      </div>
+    )
+  }
+}
+
+export default TodoList;
--- todo-list-react/src/index.js
+++ todo-list-react/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Styling
+import './index.css';
+
+// Components
+import TodoList from './TodoList';
+
+ReactDOM.render(
+  <div>
+    <TodoList />
+  </div>,
+  document.getElementById('root')
+);
